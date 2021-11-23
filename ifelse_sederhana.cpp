90f58d8b2328fa73c8eaf06b2618abf7f3e32c53
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-11-23 23:50:41 +0700

    Program C++

---
+++ ifelse_sederhana.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string kata;
+	cout<<"Masukan kata =  Sayang "<<endl;
+	cin>>kata;
+	
+	if (kata=="Sayang"){
+		cout<<"Kata yang dimasukan sesuai"<<endl;
+	}else {
+		cout<<"Kata yang dimasukan tidak sesuai"<<endl;
+	}
+
+return 0;
+}
