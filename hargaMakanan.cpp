7fc9886c25fe367e15884f9e4d05139b9063ef93
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-29 19:08:50 +0700

    Add files via upload

---
+++ hargaMakanan.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	char harga;
+	
+	cout<<"Sistem untuk mengetahui harga Makanan di Rumah Makan Royrafmp"<<endl;
+	cout<<"1. Mie Ayam"<<endl;
+	cout<<"2. Bakso"<<endl;
+	cout<<"3. Es tea"<<endl;
+	cout<<"Pilih makanan yang ingin diketahui harganya (1,2,3): ";
+	cin>>harga;
+	
+	cout<<endl;
+	switch(harga){
+		case'1':
+			cout<<"Harga Mie Ayam : Rp.7000,00"<<endl;
+			break;
+		case'2':
+			cout<<"Harga Bakso : Rp.6000,00"<<endl;
+			break;
+		case'3':
+			cout<<"Harga Es tea : Rp.2000,00"<<endl;
+			break;
+		default:
+			cout<<"INPUTAN TIDAK SESUAI KAWAN!!!";
+	}
+	return 0;
+}
