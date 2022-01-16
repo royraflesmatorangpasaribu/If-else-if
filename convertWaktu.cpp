e062c740ac6b3ae9288b40bd360dfc944c4e43f3
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2022-01-16 09:40:28 +0700

    Add files via upload

---
+++ convertWaktu.cpp
@@ -0,0 +1,42 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int N,jam,menit,detik,tes;
+    cin>> N;
+    menit = N/60;
+    tes = N%60;
+    //cout<< tes;
+    if(menit<60){
+        jam = 0;
+        detik = N%60;
+    }
+//    else if(menit==60 && detik==0){
+//        jam = 1;
+//        menit = 0;
+//		detik = 0; 
+//    }
+    else{
+    	if(detik<60){
+    		jam = 0;
+    		menit = 0;
+    		detik = N;
+		}
+		else if(detik==60){
+			jam = 0;
+			menit = 1;
+			detik = 0;
+		}
+		else{
+			jam = menit/60;
+			detik = tes;
+		}
+    }
+    cout<<jam <<" jam "<<menit<< " menit "<<detik<< " detik ";
+    return 0;
+}
